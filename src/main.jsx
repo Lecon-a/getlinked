508b06f271296b9798348b5da744c0be4b0795ed
Sunday P. Afolabi <spog4true@gmail.com>
2023-09-25 03:55:12 -0700

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <div>
+      Hi world
+    </div>
   </React.StrictMode>,
 )
